69f92fc8cf31cfaae6b58aaa5cd75a6f34ee68dc
Shreyas <shrepranav5@gmail.com>
2024-01-22 08:39:04 +0530

    Added Basic Windowing class

--- src/main/java/org/game/application/GameApplication.java
+++ src/main/java/org/game/application/GameApplication.java
@@ -1,28 +1,34 @@
 package org.game.application;
 
-
+import org.game.engine.Window;
 
 public class GameApplication implements Application {
 
     private ApplicationProperties properties;
+    private Window window;
+
     public GameApplication(ApplicationProperties properties) { this.properties = properties; }
 
     @Override
     public void onStart() {
         System.out.println("Start!");
+        window = new Window(properties);
+        window.createWindow();
     }
 
     @Override
     public void onUpdate(double timestep) {
         System.out.println("Update " + timestep);
+        window.updateWindow();
     }
 
     @Override
     public void onExit() {
         System.out.println("Exit");
+        window.destroyWindow();
     }
     @Override
     public boolean isApplicationRunning() {
-        return true;
+        return !window.isWindowClosing();
     }
 }
--- src/main/java/org/game/engine/Window.java
+++ src/main/java/org/game/engine/Window.java
@@ -1,4 +1,75 @@
 package org.game.engine;
 
+import org.lwjgl.glfw.GLFW;
+
+import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryStack.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+import org.game.application.ApplicationProperties;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.system.MemoryStack;
+
+import java.nio.IntBuffer;
+
 public class Window {
+    private ApplicationProperties properties;
+    private long windowHandle;
+    public Window(ApplicationProperties properties) { this.properties = properties; }
+
+    public void createWindow() {
+        if(!GLFW.glfwInit())
+            throw new IllegalStateException("Unable to initialize GLFW");
+
+        windowHandle = GLFW.glfwCreateWindow(properties.getWindowWidth(),
+                properties.getWindowHeight(),
+                properties.getWindowTitle(),
+                properties.isFullscreen() ? glfwGetPrimaryMonitor() : NULL,
+                NULL);
+
+        try (MemoryStack stack = stackPush() ) {
+            IntBuffer pWidth = stack.mallocInt(1); // int*
+            IntBuffer pHeight = stack.mallocInt(1); // int*
+
+            glfwGetWindowSize(windowHandle, pWidth, pHeight);
+
+            GLFWVidMode vidmode = glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor());
+
+            // Center the window
+            GLFW.glfwSetWindowPos(
+                    windowHandle,
+                    (vidmode.width() - pWidth.get(0)) / 2,
+                    (vidmode.height() - pHeight.get(0)) / 2
+            );
+        }
+
+        glfwSwapInterval(properties.isVsync() ? 1 : 0);
+        glfwShowWindow(windowHandle);
+
+        // Create OpenGL Context
+        glfwMakeContextCurrent(windowHandle);
+        GL.createCapabilities();
+    }
+
+    public void updateWindow() {
+
+        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(0.0f, 1.0f, 1.0f, 1.0f);
+
+        glfwSwapBuffers(windowHandle);
+        glfwPollEvents();
+    }
+
+    public void destroyWindow() {
+        glfwFreeCallbacks(windowHandle);
+
+        glfwDestroyWindow(windowHandle);
+        glfwTerminate();
+    }
+
+    public boolean isWindowClosing() { return glfwWindowShouldClose(windowHandle); }
 }
