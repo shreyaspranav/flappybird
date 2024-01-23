9508256a496b4702f166672659f523b155378185
Shreyas <shrepranav5@gmail.com>
2024-01-23 14:33:42 +0530

    Removed Unnessary files

--- src/main/java/org/game/engine/BufferAttributeType.java
+++
@@ -1,9 +0,0 @@
-package org.game.engine;
-
-public enum BufferAttributeType {
-    Float, Float2, Float3, Float4,
-
-    Int, Int2, Int3, Int4,
-
-    UnsignedInt, UnsignedInt2, UnsignedInt3, UnsignedInt4
-}
